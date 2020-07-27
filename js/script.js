594f0a19d05a79c5d79cd6d3b9a1f165ecadce4d
ICee-6717OK <okumuisidore@gmail.com>
2020-07-27 11:09:34 +0300

    style form

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).on("keypress", "input", function(e){
-   if(e.which == 13){
-   var name = $getElementById("#name");
-    alert(name+ " we have received your message. Thank you for reaching out to us.");
-  }
-});
